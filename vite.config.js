860281aaaf95e973658ee435d7c86291e126b8ee
rocke <=>
2025-07-13 14:27:16 +0800

    修复base路径和资源引用

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/city-weather-web/',
+  base: '/city-weather-web',
   plugins: [vue()],
 })
